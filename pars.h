c7853adf32aeb70695ddeec2c0fbe4ba07833004
mironov1105 <77550644+mironov1105@users.noreply.github.com>
2021-01-17 05:49:57 +1000

    Create pars.h

---
+++ pars.h
@@ -0,0 +1,15 @@
+#ifndef _parscpp_H
+#define _parscpp_H
+
+using namespace std;
+
+void help();
+void helpcom();
+int hesh(string word);
+void master_pass(string master, string conf, string mast);
+void get(char* text, string conf, string mast);
+void add(string text, string conf, string mast);
+void del(string text, string conf, string mast);
+void chk(string text, string conf1, string mast);
+
+#endif
